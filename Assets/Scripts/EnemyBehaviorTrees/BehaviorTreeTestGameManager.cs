52250d2a01d32d0ab4a63667a4fe4a6f6df93210
lausan3 <lauanthony4455@gmail.com>
2023-10-08 22:59:24 -0400

    Added WUG Game Manager example class for in-scene testing purposes

---
+++ Assets/Scripts/EnemyBehaviorTrees/BehaviorTreeTestGameManager.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.Linq;
+using EnemyBehaviorTrees.Agents;
+using UnityEngine;
+using WUG.BehaviorTreeVisualizer;
+
+namespace EnemyBehaviorTrees.Agents
+{
+    public class BehaviorTreeTestGameManager : MonoBehaviour
+    {
+        public static BehaviorTreeTestGameManager Instance;
+
+        public BehaviorTreeTestNPCController NPC { get; private set; }
+        private List<GameObject> waypoints = new List<GameObject>();
+        private List<GameObject> items = new List<GameObject>();
+
+        private void Awake()
+        {
+            // Singleton pattern. Ensures only one version of this lives in the scene
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void Start()
+        {
+            waypoints = GameObject.FindGameObjectsWithTag("Waypoint").ToList();
+            items = GameObject.FindGameObjectsWithTag("Item").ToList();
+
+            waypoints = waypoints.Shuffle();
+
+            NPC = FindObjectOfType<BehaviorTreeTestNPCController>();
+        }
+
+        /// <summary>
+        /// Sorts the remaining items by distance to NPC and pops the next one off the list
+        /// </summary>
+        /// <returns>Closest item</returns>
+        public GameObject GetClosestItem()
+        {
+            return items.OrderBy(x => Vector3.Distance(x.transform.position, NPC.transform.position)).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Removes an item from the list and scene
+        /// </summary>
+        /// <param name="item"></param>
+        public void PickupItem(GameObject item)
+        {
+            items.Remove(item);
+
+            Destroy(item);
+        }
+
+        /// <summary>
+        /// Finds the next waypoint on the list. This is 'random' due to shuffling on Start.
+        /// </summary>
+        /// <returns>Next waypoint</returns>
+        public GameObject GetNextWayPoint()
+        {
+            if (waypoints != null && waypoints.Count > 0)
+            {
+                GameObject nextWayPoint = waypoints[0];
+                waypoints.RemoveAt(0);
+
+                return nextWayPoint;
+            }
+
+            return null;
+        }
+
+    }
+}
