5ac8e62725d4eff327fde04ffce4af484d173e02
lausan3 <lauanthony4455@gmail.com>
2023-10-08 20:14:33 -0400

    Added namespace EnemyBehaviorTrees to Node class

--- Assets/Scripts/EnemyBehaviorTrees/Node.cs
+++ Assets/Scripts/EnemyBehaviorTrees/Node.cs
@@ -1,52 +1,55 @@
 using WUG.BehaviorTreeVisualizer;
 
-// This is the base abstract superclass for all nodes in a behavior tree - it includes:
-/*
- * public int EvaluationCount - how many times this node has been evaluated in a single run of it
- * public virtual NodeStatus Run() - overridable function to run any node's custom logic
- * public void Reset() - resets the node for another new run -- used at the end of all its logic
- * protected abstract NodeStatus OnRun() - provides any custom logic for when node is run (this is what we'll override to provide custom logic)
- * protected abstract void OnReset() - provides any custom logic for when node is reset
- */
-
-// Node class inherits:
-/*
- * public enum NodeStatus - the status of the current node with signatures: Failure, Success, Running, Unknown, and NotRun. We will use these within the code to determine what to do next within nodes.
- * public string Name - the name of the current node.
- * public string StatusReason - the reason for the current node's NodeStatus.
- * public List<NodeBase> ChildNodes - a linked list of child nodes connected to this one.
- * public NodeStatus LastNodeStatus - last node status
- */
-public abstract class Node : NodeBase
+namespace EnemyBehaviorTrees
 {
-    // Keeps track of the number of times the node has been evaluated in a single 'run'.
-    public int EvaluationCount;
+    // This is the base abstract superclass for all nodes in a behavior tree - it includes:
+    /*
+     * public int EvaluationCount - how many times this node has been evaluated in a single run of it
+     * public virtual NodeStatus Run() - overridable function to run any node's custom logic
+     * public void Reset() - resets the node for another new run -- used at the end of all its logic
+     * protected abstract NodeStatus OnRun() - provides any custom logic for when node is run (this is what we'll override to provide custom logic)
+     * protected abstract void OnReset() - provides any custom logic for when node is reset
+     */
     
-    // Runs the logic for the node
-    public virtual NodeStatus Run()
+    // Node class inherits:
+    /*
+     * public enum NodeStatus - the status of the current node with signatures: Failure, Success, Running, Unknown, and NotRun. We will use these within the code to determine what to do next within nodes.
+     * public string Name - the name of the current node.
+     * public string StatusReason - the reason for the current node's NodeStatus.
+     * public List<NodeBase> ChildNodes - a linked list of child nodes connected to this one.
+     * public NodeStatus LastNodeStatus - last node status
+     */
+    public abstract class Node : NodeBase
     {
-        // Runs the 'custom' logic
-        NodeStatus nodeStatus = OnRun();
+        // Keeps track of the number of times the node has been evaluated in a single 'run'.
+        public int EvaluationCount;
         
-        // Increments the tracker for how many times the node has been evaluated this 'run'
-        EvaluationCount++;
-        
-        // If the nodeStatus is not Running, then it is Success or Failure and can be Reset
-        if (nodeStatus != NodeStatus.Running)
+        // Runs the logic for the node
+        public virtual NodeStatus Run()
         {
-            Reset();
+            // Runs the 'custom' logic
+            NodeStatus nodeStatus = OnRun();
+            
+            // Increments the tracker for how many times the node has been evaluated this 'run'
+            EvaluationCount++;
+            
+            // If the nodeStatus is not Running, then it is Success or Failure and can be Reset
+            if (nodeStatus != NodeStatus.Running)
+            {
+                Reset();
+            }
+            
+            // Return the StatusResult.
+            return nodeStatus;
         }
         
-        // Return the StatusResult.
-        return nodeStatus;
+        public void Reset()
+        {
+             EvaluationCount = 0;
+             OnReset();
+        }
+        
+        protected abstract NodeStatus OnRun();
+        protected abstract void OnReset();
     }
-    
-    public void Reset()
-    {
-         EvaluationCount = 0;
-         OnReset();
-    }
-    
-    protected abstract NodeStatus OnRun();
-    protected abstract void OnReset();
 }
