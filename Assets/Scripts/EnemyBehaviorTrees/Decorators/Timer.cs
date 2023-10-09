992ee60143227fe4381407167dff75f575bd31d9
lausan3 <lauanthony4455@gmail.com>
2023-10-08 21:49:35 -0400

    Added Decorator type node, Timer

---
+++ Assets/Scripts/EnemyBehaviorTrees/Decorators/Timer.cs
@@ -0,0 +1,62 @@
+using WUG.BehaviorTreeVisualizer;
+using EnemyBehaviorTrees;
+using UnityEngine;
+
+namespace EnemyBehaviorTrees.Decorators
+{
+    // Timer Decorators run a node after a set amount of time expires
+    
+    public class Timer : Decorator
+    {
+        private float startTime;
+        private bool useFixedTime;
+        private float timeToWait;
+        
+        // Constructor
+        public Timer(float timeToWait, Node childNode,  bool useFixedTime = false) : 
+            base($"Timer for {timeToWait}", childNode) 
+        {
+            this.useFixedTime = useFixedTime;
+            this.timeToWait = timeToWait;
+        }
+        
+        // OnReset() - nothing to reset
+        protected override void OnReset() {}
+        
+        // OnRun() - run after set time has passed      
+        protected override NodeStatus OnRun()
+        {
+            // Confirm that a valid child node was passed in the constructor
+            if (ChildNodes.Count == 0 || ChildNodes[0] == null)
+            {
+                return NodeStatus.Failure;
+            }
+
+            // Run the child node and calculate the elapsed
+            NodeStatus originalStatus = (ChildNodes[0] as Node).Run();
+
+            // If this is the first eval, then the start time needs to be set up.
+            if (EvaluationCount == 0)
+            {
+                StatusReason = $"Starting timer for {timeToWait}. Child node status is: {originalStatus}";
+                startTime = useFixedTime ? Time.fixedTime : Time.time;
+            }
+
+            // Calculate how much time has passed
+            float elapsedTime = Time.fixedTime - startTime;
+
+            // If more time has passed than we wanted, it's time to stop
+            if (elapsedTime > timeToWait)
+            {
+                StatusReason = $"Timer complete - Child node status is: {originalStatus}";
+                return NodeStatus.Success;
+            }
+
+            // Otherwise, keep running
+            StatusReason = $"Timer is {elapsedTime} out of {timeToWait}. Child node status is: {originalStatus}";
+            return NodeStatus.Running;
+
+        }
+    }
+}
+
