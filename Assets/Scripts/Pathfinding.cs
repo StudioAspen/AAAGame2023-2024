6901176105dcaa7897246a91448dc158134a7111
justSass <yorgeliscastillo92@gmail.com>
2023-10-16 15:33:34 -0700

    Object Chases another object

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.AI;
+using UnityEngine.AI;
 
 public class Pathfinding : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject Player;
+    private Vector3 Target;
+    private NavMeshAgent agent;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player");
+        agent = gameObject.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Player != null)
+        {
+            Target = Player.transform.position;
+            agent.SetDestination(Target);
+
+        }
     }
 }
