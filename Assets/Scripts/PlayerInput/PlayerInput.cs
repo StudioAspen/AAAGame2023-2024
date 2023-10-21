4ec44027f0287f1f0d0eeab86ea8184e781a109e
TheStress <lieu.nelson386@gmail.com>
2023-10-21 19:34:17 -0400

    placeholder changes

---
+++ Assets/Scripts/PlayerInput/PlayerInput.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+
+
+
+    //Dash dash;
+    //stabanddash stabanddash;
+    //slashandslide slashandslide;
+    //movement movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //dash = GetComponent<Dash>();
+        //stabAndDash = GetComponent<StabAndDash>();
+        //slashAndSlide = GetComponent<SlashAndSlide>();
+        //movement = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            //dash.Dash();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //dash.Dash();
+        }
+    }
+
+
+}
