b5e0b57ade3d59413a0e889f602a12304c08ca20
Benicio L <benicioladines814@gmail.com>
2023-10-21 19:12:49 -0400

    created stabable script
    to be attached to things in the level the player can stab!

---
+++ Assets/Scripts/Stabable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stabable : MonoBehaviour
+{
+
+    Vector3 dashDir;
+    float dashLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+}
