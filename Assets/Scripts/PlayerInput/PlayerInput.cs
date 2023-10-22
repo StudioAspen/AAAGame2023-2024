b873bda1d122efea440ea81c68b160b9eb9e8e61
TheStress <lieu.nelson386@gmail.com>
2023-10-21 22:22:45 -0400

    added outline of intention of PlayerInput

--- Assets/Scripts/PlayerInput/PlayerInput.cs
+++ Assets/Scripts/PlayerInput/PlayerInput.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     /// <summary>
-    /// 
+    /// CURRENTLY NONE OF THESE MOVEMENTS EXIST IN THIS BRANCH BUT COMMENTED TO SHOW THE INTENTION
     /// </summary>
 
-
+    bool canMove = true;
 
     //Dash dash;
     //stabanddash stabanddash;
@@ -23,20 +23,51 @@ public class PlayerInput : MonoBehaviour
         //stabAndDash = GetComponent<StabAndDash>();
         //slashAndSlide = GetComponent<SlashAndSlide>();
         //movement = GetComponent<Movement>();
+
+        //dash.OnStartDash.AddListener(StartingMove);
+        //stabAndDash.OnStartStab.AddListener(StartingMove);
+        //slashAndSlide.OnStartSlash.AddListener(StartingMove);
+
+        //dash.OnEndDash.AddListener(EndingMove);
+        //stabAndDash.OnEndStab.AddListener(EndingMove);
+        //slashAndSlide.OnEndSlash.AddListener(EndingMove);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (canMove)
         {
-            //dash.Dash();
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            //dash.Dash();
+            //Combat Moves
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //stabAndDash.StartStab();
+            }
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                //slashAndSlide.StartSlash();
+            }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                //dash.Dash();
+            }
+
+            // Regular movement
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //movement.Jump();
+            }
+            Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            //movement.Move(direction);
         }
     }
 
-
+    public void StartingMove()
+    {
+        canMove = false;
+    }
+    public void EndingMove()
+    {
+        canMove = true;
+    }
 }
