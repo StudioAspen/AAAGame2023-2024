5c2f1c4fe1eb1ccc9fa3ef060c1334ca79a30cf7
lausan3 <lauanthony4455@gmail.com>
2023-10-25 20:21:54 -0400

    Completed better behavior tree that includes de-aggro range, attack range, and chase state

--- Assets/Scripts/EnemyBehaviorTrees/BaseEnemyNPCController.cs
+++ Assets/Scripts/EnemyBehaviorTrees/BaseEnemyNPCController.cs
@@ -4,12 +4,15 @@ using UnityEngine.AI;
 using WUG.BehaviorTreeVisualizer;
 using EnemyBehaviorTrees.Nodes;
 using EnemyBehaviorTrees.Managers;
+using UnityEngine.UI;
 
 namespace EnemyBehaviorTrees.Agents
 {
     public enum NavigationActivity
     {
+        IDLE,
         LOOK_FOR_PLAYER,
+        CHASE,
         PATROL
     }
 
@@ -20,10 +23,20 @@ namespace EnemyBehaviorTrees.Agents
         public NodeBase BehaviorTree { get; set; }
 
         [Header("Agent Modification")] 
-        [Tooltip("How far away the agent checks from itself for the player")]
-        public float playerCheckRange = 5f;
         [Tooltip("How long the agent waits at its patrol location until it checks for the player again")]
         public float patrolStayTime = 2f;
+        
+        [Tooltip("How far away the agent checks from itself for the player")]
+        public float playerAggroRange = 5f;
+        
+        [Tooltip("How far away the agent checks from itself to see if it deaggros because the player ran far enough away")]
+        public float playerDeAggroRange = 7f;
+        
+        [Tooltip("How long the agent idles for whenever it player goes into deaggro range")]
+        public float idleTimeSecs = 2f;
+
+        [Tooltip("How far away the agent needs to be to start hitting the player")]
+        public float playerHitRange = 0.5f;
 
         private Coroutine behaviorTreeRoutine;
         private YieldInstruction waitTime = new WaitForSeconds(.1f);
@@ -44,16 +57,37 @@ namespace EnemyBehaviorTrees.Agents
         private void GenerateBehaviorTree()
         {
             BehaviorTree = new Selector("Control NPC",
+                                // NAVIGATION_ACTIVITY.IDLE
+                                new Sequence("Idle",
+                                    new IsNavigationActivityTypeOf(NavigationActivity.IDLE),
+                                    new Timer(idleTimeSecs, 
+                                        new SetNavigationActivityTo(NavigationActivity.PATROL))),
+                                // NAVIGATION_ACTIVITY.LOOK_FOR_PLAYER
                                 new Sequence("Look for player",
                                     new IsNavigationActivityTypeOf(NavigationActivity.LOOK_FOR_PLAYER),
-                                    new Selector("Look for or move to player",
-                                        new Sequence("Look for player",
-                                            new Inverter("Inverter", 
-                                                new IsPlayerNearBy(playerCheckRange)),
-                                            // Player is not nearby, patrol to next point
-                                            new SetNavigationActivityTo(NavigationActivity.PATROL)),
-                                        new Sequence("Navigate to player",
-                                            new NavigateToDestination()))),
+                                    new Selector("Is player in aggro range?",
+                                        // AGENT AGGRO
+                                        new Sequence("Look for player", 
+                                            new IsPlayerNearBy(playerAggroRange),
+                                            new SetNavigationActivityTo(NavigationActivity.CHASE)),
+                                        new SetNavigationActivityTo(NavigationActivity.PATROL))),
+                                // NAVIGATION_ACTIVITY.CHASE
+                                new Sequence("Chase Player",
+                                    new IsNavigationActivityTypeOf(NavigationActivity.CHASE),
+                                    new Selector("Is player still in aggro range?",
+                                        new Sequence("Check if player still in aggro range",
+                                            new IsPlayerNearBy(playerAggroRange),
+                                            new Selector("Is player within hit range? Attack/Chase",
+                                                new Sequence("Check if player is in attack range",
+                                                    new IsPlayerNearBy(playerHitRange),
+                                                    new AttackPlayer()),
+                                                new NavigateToDestination())),
+                                        new Selector("Is player within deaggro range?",
+                                            new Sequence("Check if player in deaggro range",
+                                                new IsPlayerNearBy(playerDeAggroRange),
+                                                new SetNavigationActivityTo(NavigationActivity.IDLE)),
+                                            new SetNavigationActivityTo(NavigationActivity.LOOK_FOR_PLAYER)))),
+                                // NAVIGATION_ACTIVITY.PATROL
                                 new Sequence("Move to Waypoint",
                                     new IsNavigationActivityTypeOf(NavigationActivity.PATROL),
                                     new NavigateToDestination(),
