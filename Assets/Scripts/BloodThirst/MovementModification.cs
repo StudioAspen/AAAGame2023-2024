17c7e46dc86ba9696ad3f6b1682bede137c58062
ChillBeanJosh <joshover23@gmail.com>
2023-11-11 03:48:06 -0500

    Update MovementModification.cs
    Idea on how movement modification can be formated since their isnt a template on how it is wanted to be done

--- Assets/Scripts/BloodThirst/MovementModification.cs
+++ Assets/Scripts/BloodThirst/MovementModification.cs
@@ -6,8 +6,40 @@ public class MovementModification : MonoBehaviour
 {
     public float boostForAll; // 0-1 value represent percentage
 
+<<<<<<< Updated upstream
+=======
+    public PlayerMovement speed;
+    public float test = 0f; //placeholder for blood gauge
+    public bool boostOn = true;
+
+ 
+       
+>>>>>>> Stashed changes
     public void SetBoost(float boost)
     {
         boostForAll = boost;
     }
+<<<<<<< Updated upstream
 }
+=======
+
+    void Update()
+    {
+
+        if(test >= 5 && boostOn) //whenever blood gauge reaches over the activation theshold
+        {
+          boostOn = false;
+            speed.moveSpeed += speed.moveSpeed * boostForAll;
+        }
+
+        if(test < 5 && boostOn == false) ///whenever blood gauge reaches under the activation theshold
+        {
+            speed.moveSpeed = 5;
+            boostOn = true;
+        }
+    }
+   
+}
+
+
+>>>>>>> Stashed changes
