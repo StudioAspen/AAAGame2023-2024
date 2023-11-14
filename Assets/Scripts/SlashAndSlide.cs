881bc9caf61cf8ac501e71368ef8503edb5b3522
vincentliok <vl1042@nyu.edu>
2023-11-13 19:18:04 -0500

    Made code cleaner and incorporated movement modification

--- Assets/Scripts/SlashAndSlide.cs
+++ Assets/Scripts/SlashAndSlide.cs
@@ -9,10 +9,16 @@ public class SlashAndSlide : MonoBehaviour
 {
     public Transform sword;
     public float slideSpeed;
+    public float boostedSlideSpeed;
     public float bloodGained;
 
-    UnityEvent OnSlashStart;
-    UnityEvent OnSlashEnd;
+    public UnityEvent OnSlashStart = new UnityEvent();
+    public UnityEvent OnSlashEnd = new UnityEvent();
+
+    public UnityEvent OnSlideStart = new UnityEvent();
+    public UnityEvent OnSlideEnd = new UnityEvent();
+
+    private MovementModification movementModification;
 
     private PathCreator pathCreator;
     private EndOfPathInstruction end;
@@ -27,44 +33,59 @@ public class SlashAndSlide : MonoBehaviour
     {
         end = EndOfPathInstruction.Stop;
         rb = GetComponent<Rigidbody>();
+
+        movementModification = GetComponent<MovementModification>();
     }
 
     private void Update()
     {
         if (sliding)
         {
-            dstTravelled += slideSpeed * Time.deltaTime;
-            transform.position = pathCreator.path.GetPointAtDistance(dstTravelled, end) + playerOffset;
-            sword.transform.position = pathCreator.path.GetPointAtDistance(dstTravelled, end) + swordOffset;
-            sword.transform.up = pathCreator.path.GetNormalAtDistance(dstTravelled, end);
+            UpdateSliding();
+        }
+    }
 
-            if (dstTravelled > pathCreator.path.length)
+    private void UpdateSliding()
+    {
+        dstTravelled += Mathf.Lerp(slideSpeed, boostedSlideSpeed, movementModification.boostForAll) * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(dstTravelled, end) + playerOffset;
+        sword.transform.position = pathCreator.path.GetPointAtDistance(dstTravelled, end) + swordOffset;
+        sword.transform.up = pathCreator.path.GetNormalAtDistance(dstTravelled, end);
+
+        if (dstTravelled > pathCreator.path.length)
+        {
+            EndSlide();
+
+            if (TryGetComponent<PlayerMovement>(out PlayerMovement pm))
             {
-                dstTravelled = 0f;
-                sliding = false;
-                rb.useGravity = true;
+                pm.Jump();
+                pm.ResetJump();
+            }
 
-                if (TryGetComponent<PlayerMovement>(out PlayerMovement pm))
-                {
-                    pm.Jump();
-                    pm.ResetJump();
-                }
-
-                if (TryGetComponent<DashMovement>(out DashMovement dm)) {
-                    dm.ResetDash();
-                }
+            if (TryGetComponent<DashMovement>(out DashMovement dm))
+            {
+                dm.ResetDash();
             }
         }
     }
 
-    public void StartSlash()
+    private void StartSlide()
     {
-        if (OnSlashStart != null)
-        {
-            OnSlashStart.Invoke();
-        }
+        OnSlideStart.Invoke();
+        sliding = true;
 
-        // Start slash animation
+        if (TryGetComponent<BloodThirst>(out BloodThirst bt))
+        {
+            bt.GainBlood(bloodGained);
+        }
+    }
+
+    private void EndSlide()
+    {
+        OnSlideEnd.Invoke();
+        dstTravelled = 0f;
+        sliding = false;
+        rb.useGravity = true;
     }
 
     public void SlashContact(GameObject other, Vector3 contactPoint)
@@ -72,12 +93,7 @@ public class SlashAndSlide : MonoBehaviour
         if (other.TryGetComponent<Slashable>(out Slashable slashable) && 
             other.TryGetComponent<PathCreator>(out PathCreator pc))
         {
-            if (TryGetComponent<BloodThirst>(out BloodThirst bt))
-            {
-                bt.GainBlood(bloodGained);
-            }
-
-            sliding = true;
+            StartSlide();
             pathCreator = pc;
             rb.useGravity = false;
             dstTravelled = pathCreator.path.GetClosestDistanceAlongPath(contactPoint);
@@ -88,9 +104,9 @@ public class SlashAndSlide : MonoBehaviour
         }
     }
 
-    public void InterruptSlash()
+    public void InterruptSlide()
     {
-
+        EndSlide();
     }
 
 }
