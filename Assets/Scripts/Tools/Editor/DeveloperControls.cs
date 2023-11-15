9ee7f9948bef141d12140e863690dc8fdfed033d
getmikyled <mikylemosquera@gmail.com>
2023-11-14 19:29:19 -0800

    Update DeveloperControls.cs

--- Assets/Scripts/Tools/Editor/DeveloperControls.cs
+++ Assets/Scripts/Tools/Editor/DeveloperControls.cs
@@ -1,45 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class DeveloperControls : EditorWindow
 {
-
+    Vector2 scrollPos;
     float windowWidth;
+    float windowHeight;
 
+    string userInput = "Enter Command Here";
+    string consoleLog = "";
+
+    // Open the Editor Window
     [MenuItem("Tools/Developer Controls")]
-   public static void ShowWindow()
+    public static void ShowWindow()
     {
         EditorWindow.GetWindow<DeveloperControls>();
     }
 
-    private void OnInspectorUpdate()
+    private void OnInspectorUpdate() // To repaint the window
     {
         Repaint();
     }
 
     private void OnGUI()
     {
-        windowWidth = EditorWindow.GetWindow<DeveloperControls>().position.width;
+        windowWidth = EditorWindow.GetWindow<DeveloperControls>().position.width; // gets the window width for sizing gui elements
+        windowHeight = EditorWindow.GetWindow<DeveloperControls>().position.height; // gets the window height for sizing gui elements
 
-        string userInput = "Enter Command Here";
-        userInput = EditorGUI.TextField(new Rect(0, 0, windowWidth, 20), userInput);
+        // Clear the Console
+        if(GUILayout.Button("Clear Console"))
+        {
+            consoleLog = "";
+        }
 
+        userInput = EditorGUILayout.TextField(userInput, GUILayout.Height(20));
+
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Height(windowHeight - 20));
+            EditorGUILayout.TextArea(consoleLog, GUILayout.ExpandHeight(true));
+        EditorGUILayout.EndScrollView();
+
+        // Detects whether or not the enter key was pressed (Checks if a command was entered)
         Event eventCurrent = Event.current;
-        if (eventCurrent.type == EventType.KeyDown) {
+        if (eventCurrent.type == EventType.KeyDown)
+        {
             if (eventCurrent.keyCode == KeyCode.Return)
             {
-                GUI.Label(new Rect(0, 30, windowWidth, 20), userInput);
+                ExecuteCommand();
             }
         }
     }
 
-    void AddLine(string text)
+    private void ExecuteCommand()
     {
-        Debug.Log("Printed");
-        GUI.Label(new Rect(10,10,windowWidth, 20), text);
+        consoleLog = "> " + userInput + "\n" + consoleLog;
+        userInput = "Enter Command Here";
+        scrollPos.y = 0;
     }
 }
 
+ 
