7285bc3a4527590e238dc10c6fe86131ba53fcdb
TheStress <lieu.nelson386@gmail.com>
2023-11-18 17:31:22 -0500

    basic blood geyser functionality

--- Assets/Scripts/EnviornmentalFeatures/BloodGeyser.cs
+++ Assets/Scripts/EnviornmentalFeatures/BloodGeyser.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class BloodGeyser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    float acceleration;
+    float maxSpeed;
+    public void SetStats(float setAcceleration, float setMaxSpeed) {
+        acceleration = setAcceleration;
+        maxSpeed = setMaxSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerStay(Collider other) {
+        if(other.TryGetComponent<Rigidbody>(out Rigidbody rb)) {
+            if (rb.velocity.y < maxSpeed) {
+                rb.AddForce(Vector3.up * acceleration, ForceMode.VelocityChange);
+            }
+        }
     }
 }
--- Assets/Scripts/EnviornmentalFeatures/EyeballGeyser.cs
+++ Assets/Scripts/EnviornmentalFeatures/EyeballGeyser.cs
@@ -5,11 +5,15 @@ public class EyeballGeyser : MonoBehaviour {
     [Header("References")]
     [SerializeField] GameObject bloodGeyser;
 
-    [Header("Geyser Variables")]
+    [Header("Geyser Size")]
     [SerializeField] float height;
     [SerializeField] float radius;
     [SerializeField] float duration;
     [SerializeField] bool isInfinate;
+
+    [Header("Geyser Functionality")]
+    [SerializeField] float geyserAcceleration;
+    [SerializeField] float geyserMaxSpeed;
 
 
     // Variables
@@ -39,15 +43,17 @@ public class EyeballGeyser : MonoBehaviour {
         geyserTimer = duration;
 
         bloodGeyser.SetActive(true);
-        bloodGeyser.transform.position = transform.position + Vector3.up * (height/2);
-        bloodGeyser.transform.localScale = new Vector3(radius, height/2, radius);
+
+        //Setting position and scale
+        bloodGeyser.transform.position = transform.position + Vector3.up * (height / 2);
+        bloodGeyser.transform.localScale = new Vector3(radius, height / 2, radius);
+        bloodGeyser.GetComponent<BloodGeyser>().SetStats(geyserAcceleration, geyserMaxSpeed);
     }
     private void EndBloodGeyser() {
         isBlood = false;
 
         bloodGeyser.SetActive(false);
     }
-
     private void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position + Vector3.up * height/2, new Vector3(radius, height, radius));
         Gizmos.DrawLine(transform.position, transform.position + Vector3.up * (height - radius));
