512085824e11c655f5a7e39f735e7969e6a82e0d
vincentliok <vl1042@nyu.edu>
2023-12-11 03:51:58 -0500

    Laser, UI, keycode parameter, player input, bugfixes

--- Assets/Scripts/EnergyBlast/EnergyBlast.cs
+++ Assets/Scripts/EnergyBlast/EnergyBlast.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Rendering.Universal.Internal;
 
 public class EnergyBlast : MonoBehaviour
 {
+    [System.NonSerialized]
+    public UnityEvent OnChargesChanged = new UnityEvent();
+
+    public GameObject energyBlast;
     public RectTransform reticle;
+    public LayerMask ignoredLayers;
 
     public float maxRange;
     public int maxNumOfCharges;
     public float rechargeTimer;
     public float timeBetweenShots;
 
-    private int currNumOfCharges;
+    [System.NonSerialized]
+    public int currNumOfCharges;
+
     private float currRechargeTimer = 0f;
     private float currTimeBetweenShots;
 
@@ -20,6 +28,7 @@ public class EnergyBlast : MonoBehaviour
     {
         currNumOfCharges = maxNumOfCharges;
         currTimeBetweenShots = timeBetweenShots;
+        OnChargesChanged.Invoke();
     }
 
     private void Update()
@@ -30,33 +39,39 @@ public class EnergyBlast : MonoBehaviour
         {
             currTimeBetweenShots += Time.deltaTime;
         }
+    }
 
-        if (Input.GetMouseButtonDown(0) && currTimeBetweenShots >= timeBetweenShots && currNumOfCharges > 0)
+    public void Shoot()
+    {
+        if (currTimeBetweenShots >= timeBetweenShots && currNumOfCharges > 0)
         {
-            Shoot();
+            ShootInternal();
             currTimeBetweenShots = 0f;
             currRechargeTimer = 0f;
             currNumOfCharges--;
+            OnChargesChanged.Invoke();
         }
     }
 
-    private void Shoot()
+    private void ShootInternal()
     {
         Ray ray = Camera.main.ScreenPointToRay(reticle.position);
         float dist = Vector3.Distance(Camera.main.transform.position, transform.position);
         Vector3 startPoint = ray.GetPoint(dist);
-        Vector3 dir = Camera.main.transform.forward;
         RaycastHit hit;
-        if (Physics.Raycast(startPoint, dir, out hit, maxRange))
+        if (Physics.Raycast(startPoint, ray.direction, out hit, maxRange, ~ignoredLayers))
         {
-            Debug.DrawLine(startPoint, hit.point, Color.white, 5f);
-            Debug.Log("Found an object - distance: " + hit.distance);
+            CreateLaser(MidPoint(startPoint, hit.point), ray.direction, Vector3.Distance(startPoint, hit.point));
 
             EnergyBlastedEffect effect = hit.collider.gameObject.GetComponent<EnergyBlastedEffect>();
             if (effect)
             {
                 effect.TriggerEffect();
             }
+        }
+        else // If no objects in raycast, still shoot laser
+        {
+            CreateLaser(MidPoint(startPoint, ray.GetPoint(maxRange)), ray.direction, maxRange - dist);
         }
     }
 
@@ -72,7 +87,34 @@ public class EnergyBlast : MonoBehaviour
             {
                 currNumOfCharges++;
                 currRechargeTimer = 0f;
+                OnChargesChanged.Invoke();
             }
         }
+    }
+
+    private Vector3 MidPoint(Vector3 start, Vector3 end)
+    {
+        return new Vector3(
+            (start.x + end.x) / 2,
+            (start.y + end.y) / 2,
+            (start.z + end.z) / 2
+        );
+    }
+
+    private void CreateLaser(Vector3 pos, Vector3 rot, float dist)
+    {
+        GameObject laser = Instantiate(energyBlast, pos, Quaternion.identity);
+        laser.transform.up = rot;
+
+        float targetSize = dist;
+        float currentSize = laser.GetComponent<BoxCollider>().bounds.size.y;
+
+        Vector3 scale = laser.transform.localScale;
+
+        scale.y = targetSize * scale.y / currentSize;
+
+        laser.transform.localScale = scale;
+
+        Destroy(laser, 1f);
     }
 }
