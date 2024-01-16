275ed9cae4aeb908e76daa7db83f5ecaeec612ef
lausan3 <lauanthony4455@gmail.com>
2024-01-16 01:00:37 -0500

    Added NavigationActivity ENUM on NPCAgentBase instead of using string type for clarity and safety.

--- Assets/Scripts/EnemyBehaviorTrees/Internal/NPCAgentBase.cs
+++ Assets/Scripts/EnemyBehaviorTrees/Internal/NPCAgentBase.cs
@@ -8,6 +8,16 @@ namespace EnemyBehaviorTrees.Internal
 {
     public abstract class NPCAgentBase : MonoBehaviour, IBehaviorTree
     {
+        // Add any new Navigation Activities you have a need for here, we can't really generalize these in the interfaces like I wanted to, because interfaces
+        // can't contain enums. :(
+        public enum NavigationActivity
+        {
+            IDLE,
+            CHASE,
+            PATROL,
+            LOOK_FOR_PLAYER,
+        }
+        
         public NavMeshAgent MyNavMesh { get; private set; }
         public NodeBase BehaviorTree { get; set; }
         public Blackboard blackboard = new Blackboard();
@@ -19,10 +29,10 @@ namespace EnemyBehaviorTrees.Internal
         {
             MyNavMesh = this.GetComponent<NavMeshAgent>();
             
-            // set default navigation activity
-            blackboard.SetEntry("Navigation Activity", "Look for player");
+            // Set default navigation activity
+            blackboard.SetEntry<NavigationActivity>("Navigation Activity", NavigationActivity.LOOK_FOR_PLAYER);
             
-            Debug.Log($"Initialized NPC with Navigation activity of: {blackboard.GetEntry<string>("Navigation Activity", out string value)}");
+            Debug.Log($"Initialized {this.name} with Navigation activity of: {blackboard.GetEntry<NavigationActivity>("Navigation Activity", out bool result).ToString()}");
     
             GenerateBehaviorTree();
     
