bb5077f035a526c0cc9a398dbf70416067f05e67
MichaelPoorun <102557175+MichaelPoorun@users.noreply.github.com>
2024-02-12 00:25:59 -0500

    Changes for building

--- Assets/Scripts/EnviornmentalFeatures/Stabable.cs
+++ Assets/Scripts/EnviornmentalFeatures/Stabable.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
+//using static UnityEditor.PlayerSettings;
 
 public class Stabable : MonoBehaviour
 {
