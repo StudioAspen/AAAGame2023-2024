4177243dcf97d57ff625c4c055c6a335184612a2
Kyle Yang <112425182+kyleyang2020@users.noreply.github.com>
2024-02-25 14:39:02 -0500

    finished sausage enemy implementation (#135)
    * added different states for enemies
    
    carbon copy of the sword behavior tree,
    
    * update state comments
    
    * added transitions from each different state
    
    using raycasts
    
    * added deathstate transistion and comments
    
    comments for enemy implementation
    k to kill enemies
    
    * started to implement enemy pathfinding
    
    added some additional comments
    
    * minor comment changes, created projectile script, created a new tag for enemy spawn point
    
    * integrated Timer class into switchstate aggro
    
    * adding projectile behavior for enemy
    
    * creatd abstract slashable for enemy interaction
    
    * made stabable abstract for generic
    
    * some implementation of slash/dash of the sausage enemy
    
    and also some merge commits
    
    * fixed timer bug in state manager
    
    * unfinished stunned state for enemy
    
    * fixed dev console bug + removed old slashable script
    
    * finished stun state, polished code
    
    deletes enemy on death forever
    
    * changed prefab names, remove redundant code
    
    * updated prefabs
    
    * have all parameters into enemy
    
    * fixes
    
    - letting designers edit the distance for agro, deagro, move speed
    - shooting creation bug
    - [bullet not using try check component
    - bullet does not destory itself
    - deagro does not work as intented, added temp behavior (not according to design doc)
    
    * added AI to template scene + remove debug
    
    ---------
    
    Co-authored-by: TheStress <lieu.nelson386@gmail.com>

--- Assets/Scripts/enemyBehavior/Bullet.cs
+++ Assets/Scripts/enemyBehavior/Bullet.cs
@@ -6,9 +6,27 @@ public class Bullet : MonoBehaviour
 {
     public Vector3 moveForce;
     public Rigidbody rigidBody;
+    public float damage;
+    [SerializeField] float maxDistanceTravel;
 
+    Vector3 spawnPos;
+    private void Start() {
+        spawnPos = transform.position;
+    }
     private void FixedUpdate()
     {
-        rigidBody.AddForce(moveForce * Time.deltaTime);
+        rigidBody.velocity = moveForce;
+    }
+    private void Update() {
+        // Destorying itself when it travels far enough
+        if((spawnPos - transform.position).magnitude > maxDistanceTravel) {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player" && other.gameObject.TryGetComponent(out BloodThirst blood)) {
+            blood.LoseBlood(damage);
+            Destroy(gameObject);
+        }
     }
 }
