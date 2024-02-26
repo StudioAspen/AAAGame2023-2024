3cb8c0f2be469fe0fff9c2d11b16afbc99545905
Nelson Lieu <36254391+TheStress@users.noreply.github.com>
2024-02-25 20:12:47 -0500

    Update EnemyStateManager.cs (#137)

--- Assets/Scripts/enemyBehavior/EnemyStateManager.cs
+++ Assets/Scripts/enemyBehavior/EnemyStateManager.cs
@@ -48,6 +48,8 @@ public class EnemyStateManager : MonoBehaviour
         stunState = new EnemyStunState();
         deathState = new EnemyDeathState();
 
+        spawnpoint = transform;
+
     // setting references
     renderer = GetComponent<Renderer>();
         playerTransform = FindObjectOfType<PlayerInput>().transform;
