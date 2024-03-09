71060379f1fd69f736b96406843f4f823438157f
Benicio <benicioladines814@gmail.com>
2024-03-08 20:25:06 -0500

    rib platform basic functionality is in!
    currently the knockback box deactivates when the player touches it... should probably be tied to an animation later

--- Assets/Scripts/EnviornmentalFeatures/RibPlatform.cs
+++ Assets/Scripts/EnviornmentalFeatures/RibPlatform.cs
@@ -6,59 +6,65 @@ public class RibPlatform : MonoBehaviour
 {
 
     [Header("Variables")]
-    [SerializeField] float launchForce;
-    [SerializeField] float waitTime;
-    [SerializeField] float timeClosed;
-    [SerializeField] bool ribsOpen;
-    [SerializeField] bool waitingToClose;
-    [SerializeField] Collider knockbackTrigger;
-    [SerializeField] Collider openCollider;
-    [SerializeField] Collider closedCollider;
+    [SerializeField] float launchForce; //force applied to player when the ribs close
+    [SerializeField] float waitTime; //time the platform waits before closing
+    [SerializeField] float timeClosed; //time the platform is closed before reopening
+    bool ribsOpen; //whether the ribs r open or not, just for testing right now
+    [SerializeField] bool waitingToClose; //set to true as soon as the player lands on the platform for the first time
+    [SerializeField] Collider knockbackTrigger;//trigger collider that pushes the player away when the platform closes
+    [SerializeField] Collider openCollider;//platform collider when ribs r open
+    [SerializeField] Collider closedCollider;//platform collider when ribs r closed (should probably be round so the player cant stand on it)
+    MeshRenderer renderer; //using to change the platform's color when open/closed
 
-    Timer timer = new Timer();
+    Timer timerTillClosed = new Timer();
+    Timer timerTillOpened = new Timer();
 
     // Start is called before the first frame update
     void Start()
     {
-        if (ribsOpen)
-        {
-            OpenRibs();
-        }
-        else
-        {
-            CloseRibs();
-        }
+        renderer= GetComponent<MeshRenderer>();
     
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer.UpdateTimer();
-        
+        timerTillClosed.UpdateTimer();
+        timerTillOpened.UpdateTimer();
+        if(ribsOpen)
+        {
+            renderer.material.color = Color.red;
+        }
+        else
+        {
+            renderer.material.color = Color.green;
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
 
-        if (ribsOpen && !waitingToClose)
+        if (collision.gameObject.TryGetComponent<PlayerInput>(out PlayerInput input))
         {
-            Debug.Log("timer started");
-            timer.StartTimer(waitTime, CloseRibs);
-            waitingToClose = true;
+            if (!waitingToClose) //if the player hasnt stepped on the platform yet, this starts the timer for when the platform closes
+            {
+                timerTillClosed.StartTimer(waitTime, CloseRibs);
+                waitingToClose = true;
+            }
         }
+        
     }
 
     private void CloseRibs()
     {
-        Debug.Log("closing");
+        timerTillOpened.StartTimer(timeClosed, OpenRibs);
         knockbackTrigger.enabled = true;
         openCollider.enabled = false;
         //play closing animation
-        knockbackTrigger.enabled = false;
         closedCollider.enabled = true;
         ribsOpen = false;
-        timer.StartTimer(timeClosed, OpenRibs);
+        
     }
 
     private void OpenRibs()
@@ -73,10 +79,19 @@ public class RibPlatform : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        /*if(other.TryGetComponent<Rigidbody>(out Rigidbody rb))
         {
             Debug.Log("hit!");
+            knockbackTrigger.enabled = false;
+        }*/
+
+        if (other.TryGetComponent<PlayerInput>(out PlayerInput playerInput))
+        {
+            Rigidbody rb = playerInput.GetComponent<Rigidbody>();
+            playerInput.DisableInput();//player input is disabled for an instant so we can apply a knockback force to 'em
             rb.AddForce(Vector3.Normalize(-rb.transform.forward + transform.up) * launchForce, ForceMode.Impulse);
+            knockbackTrigger.enabled = false; //should be disabled by the animation instead I think,, once we have it,,,
+            playerInput.EnableInput();
         }
     }
 }
